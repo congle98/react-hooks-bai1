3656868f8bd3ada60f553f8a4e72bd642cb1c92b
congle98 <79907265+congle98@users.noreply.github.com>
2021-09-16 17:16:48 +0700

    oke

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -2,7 +2,6 @@ import {TOGGLE_AUTH} from "./types";
 
 export const authReducer = (state,action) => {
     const {type} = action
-    console.log(state)
     switch (type){
         case TOGGLE_AUTH:
             return !state
